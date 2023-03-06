4ac7a70911499ba7902fb310b63ad776bc374928
Will <110594293+willro93@users.noreply.github.com>
2023-03-06 03:31:25 -0600

    se agrego colores de MUI

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,6 +3,12 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+import { createTheme, ThemeProvider } from '@mui/material/styles';  //Se agregan para poder agregar otro tipo de colores en MUI
+import { green } from '@mui/material/colors'; //Se importa un color en específico
+
+import style from "./ItemCount.module.css";
+
+
 
 const ItemCount = () => {
   const [count, setCount] = useState(0);
@@ -16,10 +22,20 @@ const ItemCount = () => {
       //alert("min limit reached");
     }
   };
+
+  const theme = createTheme({
+    palette: {
+        primary: {
+          main: '#43a047',
+        },
+        secondary: green,
+      },
+  });  
+
   return (
     <>
       <div className="main_div">
-        <div className="center_div">
+        <div className={style.centerdiv}>
           <h1>{count}</h1>
           <div className="btn_div">
             <Tooltip title="Agregar Productos">
@@ -31,6 +47,9 @@ const ItemCount = () => {
               <RemoveIcon />
             </Button>
           </div>
+          <ThemeProvider theme={theme}> 
+          <Button variant="contained" color="primary" >Agregar al carrito</Button>
+          </ThemeProvider>
         </div>
       </div>
     </>
