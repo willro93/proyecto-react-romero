d3f4410b51129912e4292df069aa127bf5a760fb
Will <110594293+willro93@users.noreply.github.com>
2023-04-05 00:31:19 -0600

    se creó la carpeta context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import Cart from "./components/Cart/Cart";
+import Form from "./components/Form/Form";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
@@ -14,6 +15,7 @@ function App() {
         <Route path="/item/:id" element={<ItemDetailContainer />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/formulario" element={<Form />} />
         <Route path="*" element={<h1> error 404: Not found </h1>} />
       </Routes>
     </BrowserRouter>
